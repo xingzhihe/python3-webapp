0561a43b0f73a762ed4e686c2ca1ef3bb06b7439
邢志和 <xingzhihe@foresee.com.cn>
2019-01-03 19:46:28 +0800

    增加json保存落盘和图表显示功能

--- www/controllers/metadata_controller.py
+++ www/controllers/metadata_controller.py
@@ -13,7 +13,7 @@ from com.phoenix.models import DataSource
 from com.phoenix.common import get_page_index
 
 @post('/api/metadata/analyse/{dsId}/{db}')
-async def api_datasource_create(request, *, dsId,db,tables):
+async def api_metadata_analyse(request, *, dsId,db,tables):
     ds = await DataSource.find(dsId)
     dsMetadata = ds.get_MetadataSource()
     from com.phoenix.connections.connectionFactory import get_Connection
@@ -23,3 +23,28 @@ async def api_datasource_create(request, *, dsId,db,tables):
     for item in arr:
         result[item["db_name"] + '.' + item["table_name"]]= dict(num_files=item["num_files"], num_rows=item["num_rows"], total_size=item["total_size"])
     return result
+
+@get('/api/metadata/setting')
+async def get_metadata_setting(request):
+    import json
+    json_file=getMetadataSettingJsonFile()
+    with open(json_file) as fr:  
+        settings = json.load(fr)
+    return settings
+
+@post('/api/metadata/setting')
+async def save_metadata_setting(request, *, settings):
+    import json
+    json_file=getMetadataSettingJsonFile()
+    with open(json_file, 'w') as fw:  
+        json.dump(settings, fw, ensure_ascii=False)
+    return {'result': 'ok'}
+
+def getMetadataSettingJsonFile():
+    import os
+    current_file = os.path.abspath(__file__)
+    current_dir = os.path.abspath(os.path.dirname(current_file))
+    json_file=os.path.join(current_dir,'..', 'static','js','metadata.settings.json')
+    if not os.path.exists(json_file):
+        os.system(r'touch %s' % json_file)
+    return json_file
